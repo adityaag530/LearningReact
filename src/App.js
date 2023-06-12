1b469d95d9370f8eada04cb2e1c64b312c88afd2
Aditya <agaditya530@gmail.com>
2023-06-12 21:45:18 +0530

    under the hood every file is using the import react library so that ther are converting jsx to html

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import Expenses from './components/Expenses'
+import React from 'react';
+import Expenses from './components/Expenses';
+
 
 function App() {
   const expenses = [
@@ -27,6 +29,15 @@ function App() {
       date: new Date(2023, 6, 23),
     },
   ];
+
+  //old way of doing
+  // return React.createElement(
+  //   'div',
+  //   {}, 
+  //   React.createElement('h2', {}, "Let's get Started!"),
+  //   React.createElement(Expenses, { items:expenses } )
+  // );
+
   return (
     <div className="App">
       <h1>Learning React!</h1>
